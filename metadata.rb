5a3e7e45013fcaf9d86427e0638cd64aaa5d8884
Ralf Haferkamp <rhafer@suse.de>
2012-06-14 15:20:39 +0200

    Remove apt requirements, adjust packagenames

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures ceph"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
-depends          "apt"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,13 @@
 
-apt_repository "ceph" do
-  uri "http://ceph.newdream.net/debian/"
-  distribution node['lsb']['codename'] 
-  components ["main"]
-  key "https://raw.github.com/ceph/ceph/master/keys/release.asc"
-  action :add
-end
+#apt_repository "ceph" do
+#  uri "http://ceph.newdream.net/debian/"
+#  distribution node['lsb']['codename'] 
+#  components ["main"]
+#  key "https://raw.github.com/ceph/ceph/master/keys/release.asc"
+#  action :add
+#end
 
-%w(ceph ceph-common).each do |pkg|
+%w(ceph).each do |pkg|
   package pkg do
     action :upgrade
   end
