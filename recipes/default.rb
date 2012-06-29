7d7b65da6ecd2232e67d93ddf832c8654bfa9166
Stefan Fent <sf@suse.de>
2012-06-29 10:32:04 +0200

    add all local osds to config file

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,8 +15,10 @@ end
 
 
 mon_nodes = get_mon_nodes()
+osd_devs = get_local_osds()
 
 ceph_config "/etc/ceph/ceph.conf" do
     monitors mon_nodes
+    osd_data osd_devs
     clustername node[:ceph][:clustername]
 end
