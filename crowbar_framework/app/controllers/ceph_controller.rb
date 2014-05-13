30df3bd9463cddb8f08040f3568b3810a98db8ad
Thomas Boerger <tboerger@suse.de>
2014-05-13 17:08:51 +0200

    Added strong parameters method

--- crowbar_framework/app/controllers/ceph_controller.rb
+++ crowbar_framework/app/controllers/ceph_controller.rb
@@ -23,4 +23,8 @@ class CephController < BarclampController
   def initialize_service
     @service_object = CephService.new logger
   end
+
+  def permitted_params
+    params.require(:ceph)
+  end
 end
